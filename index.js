0fb9e91f5e4251c9d73e3bd0a92a9a2500cf54b0
Allison Tzadok <allisontzadok@gmail.com>
2017-11-30 17:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function logWhisper(string){
 
 var grandmaGoals = sayHiToGrandma()
 
-if (grandmaGoals === "hello"){return sayHiToGrandma.toLowerCase()
+if (grandmaGoals === "hello"){
+  return sayHiToGrandma.toLowerCase()
 else if (grandmaGoals === "HELLO"){return sayHiToGrandma.toUpperCase(){else return sayHiToGrandma('I love you, Grandma')
 }}}
 
